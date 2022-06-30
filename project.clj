a0ea3bdf235a3b3098c49f5d1e7f21c74683be6b
Joe Robinson <joseph28robinson@gmail.com>
2022-06-30 21:19:58 +0100

    Use deps for tests

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject rightmove-alerter "0.1.0-SNAPSHOT"
-  :dependencies [[amazonica "0.3.161"]
-                 [com.amazonaws/aws-lambda-java-runtime-interface-client "2.0.0"]
-                 [com.taoensso/timbre "5.2.1"]
-                 [enlive "1.1.6"]
-                 [cheshire "5.11.0"]
-                 [org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns core.rightmove-alerter.core}
-  :profiles {:uberjar {:aot :all}})
